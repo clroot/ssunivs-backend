e5e6a022681a25c0f4d78cb331123d38f1b94155
clroot <abcdkh1209@gmail.com>
2022-01-31 12:37:10 +0900

    change jwt token issuer

--- src/lib/token.js
+++ src/lib/token.js
@@ -12,7 +12,7 @@ const jwtSecretKey = process.env.JWT_SECRET_KEY || 'JWT_SECRET';
  */
 export const generateToken = (payload, options = { expiresIn: '7d' }) => {
   const jwtOptions = {
-    issuer: '*.clroot.io', expiresIn: '7d', ...options,
+    issuer: '*.ssunivs.com', expiresIn: '7d', ...options,
   };
 
   return jwt.sign(payload, jwtSecretKey, jwtOptions);
